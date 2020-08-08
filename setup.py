6ad64547ef401ba708ff861f38d25218dc40ff70
elerac <maenosuke079@gmail.com>
2020-08-08 22:27:18 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='polanalyser',
+    version='1.0.0',
+    description='Polarization image analysis tool',
+    url='https://github.com/elerac/polanalyser',
+    author='Ryota Maeda',
+    author_email='maeda.ryota.elerac@gmail.com',
+    license='MIT',
+    packages=find_packages()
+)
