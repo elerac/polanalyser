5efb2c6645310e113e6062c50221226b9f7f4e7c
elerac <maenosuke079@gmail.com>
2020-08-11 18:54:54 +0900

    Update example_polarizationimage.py

--- example_polarizationimage.py
+++ example_polarizationimage.py
@@ -8,6 +8,11 @@ def imnormarize(image, max_src, max_dst, dtype):
     image = image.astype(np.float128)
     return np.clip(image/max_src*max_dst, 0, max_dst).astype(dtype)
 
+def applyGamma(image_uint8, gamma=2.2):
+    image_float64 = image_uint8.astype(np.float64)
+    image_applyed_gamma = (((image_float64/255.0)**(1/gamma)) * 255).astype(np.uint8)
+    return image_applyed_gamma 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file_path", type=str, help="bayer image path")
@@ -39,17 +44,21 @@ def main():
     img_Imin = pa.cvtStokesToImin(img_stokes)
     img_AoLP = pa.cvtStokesToAoLP(img_stokes)
     img_DoLP = pa.cvtStokesToDoLP(img_stokes)
+    img_AoLP_col = pa.applyLightColorToAoLP(img_AoLP)
     img_AoLP_DoLP = pa.applyLightColorToAoLP(img_AoLP, img_DoLP)
-    #img_specular = np.clip((pol.Imax-pol.Imin)/2.0, 0, maximum_value).astype(dtype)
+    img_specular = pa.cvtStokesToSpecular(img_stokes)
+    img_diffuse = pa.cvtStokesToDiffuse(img_stokes)
 
     #Export images
-    cv2.imwrite(name+"_intensity"+ext, imnormarize(img_intensity, max_val, max_val, dtype))
+    cv2.imwrite(name+"_intensity"+ext, applyGamma(imnormarize(img_intensity, max_val, max_val, dtype)))
     cv2.imwrite(name+"_max"      +ext, imnormarize(img_Imax, max_val, max_val, dtype))
     cv2.imwrite(name+"_min"      +ext, imnormarize(img_Imin, max_val, max_val, dtype))
     #cv2.imwrite(name+"_AoLP"  +".png", imnormarize(img_AoLP, np.pi, 255, np.uint8))
     cv2.imwrite(name+"_DoLP"  +".png", imnormarize(img_DoLP, 1, 255, np.uint8))
     cv2.imwrite(name+"_AoLP+DoLP"  +".png", img_AoLP_DoLP)
-    #cv2.imwrite(name+"_specular"+".png", img_specular)
+    cv2.imwrite(name+"_AoLP"  +".png", img_AoLP_col)
+    cv2.imwrite(name+"_specular"+".png", imnormarize(img_specular, max_val, max_val, dtype))
+    cv2.imwrite(name+"_diffuse"+".png", imnormarize(img_diffuse, max_val, max_val, dtype))
 
 if __name__=="__main__":
     main()
