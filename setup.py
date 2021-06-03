801c6ed71d635dfc20e1514f76408133956c8d9d
elerac <maenosuke079@gmail.com>
2021-06-03 13:51:51 +0900

    1.1.1 -> 1.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='polanalyser',
-    version='1.1.1',
+    version='1.2.0',
     description='Polarization image analysis tool',
     url='https://github.com/elerac/polanalyser',
     author='Ryota Maeda',
