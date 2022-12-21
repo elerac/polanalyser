a1bc09a43b37cfad7bd02e68b6d7225517a16d7a
Ryota MAEDA <maenosuke079@gmail.com>
2022-12-21 23:03:54 +0900

    Update demosaicing.py

--- polanalyser/demosaicing.py
+++ polanalyser/demosaicing.py
@@ -1,83 +1,83 @@
+from dataclasses import dataclass
 import cv2
 import numpy as np
 
-COLOR_PolarRGB = "COLOR_PolarRGB"
-COLOR_PolarMono = "COLOR_PolarMono"
 
-def demosaicing(img_raw, code=COLOR_PolarMono):
+@dataclass
+class ColorConversionCode:
+    is_color: bool
+    suffix: str  # suffix for OpenCV's ColorConversionCodes (i.e. "", "_VNG", "_EA")
+
+
+# Bilinear interpolation
+COLOR_PolarRGB = ColorConversionCode(is_color=True, suffix="")
+COLOR_PolarMono = ColorConversionCode(is_color=False, suffix="")
+# Variable Number of Gradients
+COLOR_PolarRGB_VNG = ColorConversionCode(is_color=True, suffix="_VNG")
+COLOR_PolarMono_VNG = ColorConversionCode(is_color=False, suffix="_VNG")
+# Edge-Aware
+COLOR_PolarRGB_EA = ColorConversionCode(is_color=True, suffix="_EA")
+COLOR_PolarMono_EA = ColorConversionCode(is_color=False, suffix="_EA")
+
+
+def demosaicing(img_raw: np.ndarray, code: ColorConversionCode = COLOR_PolarMono) -> np.ndarray:
     """Polarization demosaicing
-    
+
     Parameters
     ----------
-    img_raw : np.ndarry, (height, width)
-      RAW polarization image taken with polarizatin camera 
-      (e.g. IMX250MZR or IMX250MYR sensor)
-    code : str (optional)
-      COLOR_PolarMono or COLOR_PolarRGB
-    
+    img_raw : np.ndarray
+        Polarization image taken with polarizatin sensor (e.g. IMX250MZR or IMX250MYR sensor). The shape is (height, width).
+    code : ColorConversionCode, optional
+        Color space conversion code, by default `pa.COLOR_PolarMono`
+
     Returns
     -------
-    img_polarization: np.ndarray
-        Dmosaiced image. 0-45-90-135.
+    img_demosaiced : np.ndarray
+        Dmosaiced image. The shape is (height, width, 4). 0-45-90-135.
     """
-    if code == COLOR_PolarMono:
-        if img_raw.dtype == np.uint8 or img_raw.dtype == np.uint16:
-            return __demosaicing_mono_uint(img_raw)
-        else:
-            return __demosaicing_mono_float(img_raw)
-    elif code == COLOR_PolarRGB:
-        if img_raw.dtype == np.uint8 or img_raw.dtype == np.uint16:
-            return __demosaicing_color(img_raw)
-        else:
-            raise TypeError("dtype of `img_raw` must be np.uint8 or np.uint16")
+    if not isinstance(code, ColorConversionCode):
+        raise TypeError(f"The type of 'code' must be 'ColorConversionCode', not {type(code)}")
+
+    dtype = img_raw.dtype
+
+    if np.issubdtype(dtype, np.floating):
+        # If the dtype is floting type, the image is converted into `uint16` to apply demosaicing process.
+        # It may cause inaccurate result.
+        scale = 65535.0 / np.max(img_raw)
+        img_raw_u16 = np.clip(img_raw * scale, 0, 65535).astype(np.uint16)
+        img_demosaiced_u16 = demosaicing(img_raw_u16, code)
+        img_demosaiced = (img_demosaiced_u16 / scale).astype(img_raw.dtype)
+        return img_demosaiced
+
+    if dtype not in [np.uint8, np.uint16]:
+        raise TypeError(f"The dtype of input image must be `np.uint8` or `np.uint16`, not `{dtype}`")
+
+    if code.is_color:
+        return __demosaicing_color(img_raw, code.suffix)
     else:
-        raise ValueError(f"`code` must be {COLOR_PolarMono} or {COLOR_PolarRGB}")
+        return __demosaicing_mono(img_raw, code.suffix)
 
-def __demosaicing_mono_uint(img_mpfa):
-    """Polarization demosaicing for np.uint8 or np.uint16 type
-    """
-    img_debayer_bg = cv2.cvtColor(img_mpfa, cv2.COLOR_BayerBG2BGR)
-    img_debayer_gr = cv2.cvtColor(img_mpfa, cv2.COLOR_BayerGR2BGR)
-    img_0,  _, img_90  = np.moveaxis(img_debayer_bg, -1, 0)
-    img_45, _, img_135 = np.moveaxis(img_debayer_gr, -1, 0)
-    img_polarization = np.array([img_0, img_45, img_90, img_135], dtype=img_mpfa.dtype)
-    img_polarization = np.moveaxis(img_polarization, 0, -1)
-    return img_polarization
 
-def __demosaicing_mono_float(img_mpfa):
-    """Polarization demosaicing for arbitrary type
-   
-    cv2.cvtColor supports either uint8 or uint16 type. 
-    Float type bayer is demosaiced by this function.
-    
-    Notes
-    -----
-    pros: float available
-    cons: slow
-    """
-    height, width = img_mpfa.shape[:2]
-    img_subsampled = np.zeros((height, width, 4), dtype=img_mpfa.dtype)
-    
-    img_subsampled[0::2, 0::2, 0] = img_mpfa[0::2, 0::2]
-    img_subsampled[0::2, 1::2, 1] = img_mpfa[0::2, 1::2]
-    img_subsampled[1::2, 0::2, 2] = img_mpfa[1::2, 0::2]
-    img_subsampled[1::2, 1::2, 3] = img_mpfa[1::2, 1::2]
-    
-    kernel = np.array([[1/4, 1/2, 1/4],
-                       [1/2, 1.0, 1/2], 
-                       [1/4, 1/2, 1/4]])
-    
-    img_polarization = cv2.filter2D(img_subsampled, -1, kernel)
-    
-    return img_polarization[..., [3, 1, 0, 2]]
+def __demosaicing_mono(img_mpfa: np.ndarray, suffix: str = "") -> np.ndarray:
+    """Polarization demosaicing for np.uint8 or np.uint16 type"""
+    code_bg = getattr(cv2, f"COLOR_BayerBG2BGR{suffix}")
+    code_gr = getattr(cv2, f"COLOR_BayerGR2BGR{suffix}")
+    img_debayer_bg = cv2.cvtColor(img_mpfa, code_bg)
+    img_debayer_gr = cv2.cvtColor(img_mpfa, code_gr)
+    img_000, _, img_090 = np.moveaxis(img_debayer_bg, -1, 0)
+    img_045, _, img_135 = np.moveaxis(img_debayer_gr, -1, 0)
+    img_demosaiced = np.array([img_000, img_045, img_090, img_135], dtype=img_mpfa.dtype)
+    img_demosaiced = np.moveaxis(img_demosaiced, 0, -1)
+    return img_demosaiced
 
-def __demosaicing_color(img_cpfa):
-    """Color-Polarization demosaicing for np.uint8 or np.uint16 type
-    """
+
+def __demosaicing_color(img_cpfa: np.ndarray, suffix: str = "") -> np.ndarray:
+    """Color-Polarization demosaicing for np.uint8 or np.uint16 type"""
     height, width = img_cpfa.shape[:2]
 
     # 1. Color demosaicing process
     img_mpfa_bgr = np.empty((height, width, 3), dtype=img_cpfa.dtype)
+    code = getattr(cv2, f"COLOR_BayerBG2BGR{suffix}")
     for j in range(2):
         for i in range(2):
             # (i, j)
@@ -87,14 +87,15 @@ def __demosaicing_color(img_cpfa):
             # Down sampling ↓2
             img_bayer_ij = img_cpfa[j::2, i::2]
             # Color demosaicking
-            img_bgr_ij = cv2.cvtColor(img_bayer_ij, cv2.COLOR_BayerBG2BGR)
+            img_bgr_ij = cv2.cvtColor(img_bayer_ij, code)
             # Up samping ↑2
             img_mpfa_bgr[j::2, i::2] = img_bgr_ij
 
     # 2. Polarization demosaicing process
-    img_bgr_polarization = np.empty((height, width, 3, 4), dtype=img_mpfa_bgr.dtype)
+    img_bgr_demosaiced = np.empty((height, width, 3, 4), dtype=img_mpfa_bgr.dtype)
+    code = ColorConversionCode(is_color=False, suffix=suffix)
     for i, img_mpfa in enumerate(cv2.split(img_mpfa_bgr)):
-        img_demosaiced = demosaicing(img_mpfa, COLOR_PolarMono)
-        img_bgr_polarization[..., i, :] = img_demosaiced
+        img_demosaiced = demosaicing(img_mpfa, code)
+        img_bgr_demosaiced[..., i, :] = img_demosaiced
 
-    return img_bgr_polarization
+    return img_bgr_demosaiced
