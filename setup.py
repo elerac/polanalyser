f4145c295abc1fdb7ad760217874d2c5fed7500e
Ryota MAEDA <maenosuke079@gmail.com>
2022-12-24 21:50:16 +0900

    Add long_description in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="polanalyser",
     version="1.2.0",
     description="Polarization image analysis tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/elerac/polanalyser",
     author="Ryota Maeda",
     author_email="maeda.ryota.elerac@gmail.com",
