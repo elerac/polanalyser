fcd60ce555cef129da865b425d2ee65618e3b021
Ryota MAEDA <maenosuke079@gmail.com>
2022-12-24 21:54:11 +0900

    1.2.0 -> 2.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="polanalyser",
-    version="1.2.0",
+    version="2.0.0",
     description="Polarization image analysis tool",
     long_description=long_description,
     long_description_content_type="text/markdown",
