b1654c72a3c8f6c6dc01f0a9870d158acf86dd95
Ryota MAEDA <maenosuke079@gmail.com>
2022-12-24 21:37:34 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='polanalyser',
-    version='1.2.0',
-    description='Polarization image analysis tool',
-    url='https://github.com/elerac/polanalyser',
-    author='Ryota Maeda',
-    author_email='maeda.ryota.elerac@gmail.com',
-    license='MIT',
+    name="polanalyser",
+    version="1.2.0",
+    description="Polarization image analysis tool",
+    url="https://github.com/elerac/polanalyser",
+    author="Ryota Maeda",
+    author_email="maeda.ryota.elerac@gmail.com",
+    license="MIT",
     packages=find_packages(),
-    zip_safe=False
 )
