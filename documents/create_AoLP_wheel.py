c15d863e7b6630e3c092bffc9869148a11481015
Ryota MAEDA <maenosuke079@gmail.com>
2022-12-25 19:21:01 +0900

    Update create_AoLP_wheel.py

--- documents/create_AoLP_wheel.py
+++ documents/create_AoLP_wheel.py
@@ -1,71 +1,124 @@
-"""
-Create a color map of the AoLP
-"""
-import matplotlib.pyplot as plt
 import numpy as np
-import cv2
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.patheffects import withStroke
+import polanalyser as pa
 
-def plot_hsv_wheel(filename, is_saturation=False, is_value=False, DoLP_tick=False, dpi=300):
-    """
-    Plot HSV wheel
 
-    H : AoLP
-    S : DoLP or constant value
-    V : DoLP or constant value
-    
-    reference: https://salad-bowl-of-knowledge.github.io/hp/python/2020/01/13/cieluv.html
-    """
-    def func(r_norm, theta_norm, c, is_saturation=False, is_value=False):
-        """
-        Function for HSV mapping
-        """
-        # Calcukate AoLP and DoLP
-        AoLP = np.mod(theta_norm*358, 179)
-        DoLP = r_norm*255
-        # Hue and Saturation and Value
-        hue = AoLP
-        saturation = DoLP*is_saturation + 255*(1-is_saturation)
-        value = DoLP*is_value + 255*(1-is_value)
-        return hue*(c==0) + saturation*(c==1) + value*(c==2)
-    
-    N_theta = 1000
-    N_r = 256
+def save_wheel_reference_color(filename: str, is_saturation: bool = False, is_value: bool = False) -> None:
+    # Create a background image
+    res = 1024
+    radius = 0.5 * res
+    img_r = np.empty((res, res), dtype=np.float64)
+    img_theta = np.empty((res, res), dtype=np.float64)
+    img_mask = np.empty((res, res), dtype=np.uint8)
+    for j in range(res):
+        for i in range(res):
+            x = i - 0.5 * res
+            y = -(j - 0.5 * res)
+            r = np.sqrt(x**2 + y**2) / radius
+            theta = np.arctan2(y, x)
 
-    hsv = np.fromfunction(lambda r,theta,c: func(r/(N_r-1), theta/(N_theta-1), c, is_saturation, is_value), (N_r, N_theta, 3), dtype=np.float64).astype(np.uint8)
+            img_r[j, i] = r
+            img_theta[j, i] = np.mod(theta, np.pi)
 
-    rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)/255.0
+    # Mask of in/out wheel
+    img_inside_wheel = img_r < 1
+    img_outside_wheel = np.logical_not(img_inside_wheel)
 
-    # hue wheel plot
-    ax = plt.subplot(111, polar=True)
-    if DoLP_tick:
-        ax.set_yticks([0, 0.5, 1])
+    # Convert theta and r to colored image
+    if is_saturation == False and is_value == False:
+        saturation = 1.0
+        value = 1.0
+    elif is_saturation == True and is_value == False:
+        saturation = img_r
+        value = 1.0
+    elif is_saturation == False and is_value == True:
+        saturation = 1.0
+        value = img_r
     else:
-        ax.set_yticks([])
-    
-    #get coordinates:
-    theta = np.linspace(0, 2*np.pi, rgb.shape[1]+1)
-    r = np.linspace(0, 1, rgb.shape[0]+1)
-    Theta,R = np.meshgrid(theta, r)
-     
-    # get color
-    color = rgb.reshape((rgb.shape[0]*rgb.shape[1], rgb.shape[2]))
-    m = plt.pcolormesh(Theta, R, rgb[:,:,0], color=color, linewidth=0)
+        raise ValueError
 
-    # This is necessary to let the `color` argument determine the color
-    m.set_array(None)
-    plt.savefig(filename, bbox_inches='tight', dpi=dpi)
+    img_wheel_rgb = pa.applyColorToAoLP(img_theta, saturation, value)[:, :, ::-1]
 
-    plt.close('all')
+    # Alpha
+    img_wheel_alpha = np.empty((res, res), dtype=np.uint8)
+    img_wheel_alpha[img_inside_wheel] = 255
+    img_wheel_alpha[img_outside_wheel] = 0  # Transparent background
+
+    # Merge RGB and Alpha
+    img_wheel_rgba = np.empty((res, res, 4), dtype=np.uint8)
+    img_wheel_rgba[:, :, :3] = img_wheel_rgb
+    img_wheel_rgba[:, :, 3] = img_wheel_alpha
+
+    # Plot
+    fig = plt.figure()
+
+    # Plot background image
+    ax0 = fig.add_subplot(111)
+    ax0.imshow(img_wheel_rgba)
+    ax0.axis("off")
+
+    # Plor polar axis
+    ax1 = fig.add_subplot(111, polar=True)
+    ax1.grid(False, axis="both")  # Delete grid
+    ax1.set_yticks([])
+    # ax1.set_yticks([0, 0.5, 1.0])
+
+    # Set stroke
+    text_color = matplotlib.colors.hex2color(plt.rcParams["text.color"])
+    foreground_color = tuple(1 - np.array(text_color))
+    path_effects = [withStroke(linewidth=1, foreground=foreground_color)]
+
+    for label in ax1.get_xticklabels():
+        label.set_path_effects(path_effects)
+
+    for label in ax1.get_yticklabels():
+        label.set_path_effects(path_effects)
+
+    # Save
+    fig.savefig(filename, bbox_inches="tight", transparent=True)
+    plt.close("all")
+
 
 def main():
-    print("AoLP(Hue) Color Map")
-    plot_hsv_wheel("AoLP_wheel.jpg")
+    # Output filename settings
+    name = "AoLP_wheel"
+    ext = ".svg"
 
-    print("AoLP(Hue)+DoLP(Saturation) Color Map")
-    plot_hsv_wheel("AoLP_wheel_saturation.jpg", is_saturation=True, DoLP_tick=True)
+    style_list = ["default", "dark_background"]
+    for style in style_list:
+        for is_saturation in [False, True]:
+            for is_value in [False, True]:
+                if is_saturation and is_value:
+                    continue
 
-    print("AoLP(Hue)+DoLP(Value) Color Map")
-    plot_hsv_wheel("AoLP_wheel_value.jpg", is_value=True, DoLP_tick=True)
+                # Matplotlib settings
+                plt.style.use(style)
+                plt.rcParams["font.size"] = 14
+                plt.rcParams["font.weight"] = "normal"
 
-if __name__=="__main__":
+                # Filename
+                if is_saturation == False and is_value == False:
+                    modulation_type = ""
+                elif is_saturation == True and is_value == False:
+                    modulation_type = "_saturation"
+                elif is_saturation == False and is_value == True:
+                    modulation_type = "_value"
+
+                if style == "default":
+                    style_type = "_light"
+                elif style == "dark_background":
+                    style_type = "_dark"
+
+                filename = f"{name}{modulation_type}{style_type}{ext}"
+
+                # Generate wheel and save
+                print(f"Export '{filename}'")
+                save_wheel_reference_color(filename, is_saturation, is_value)
+
+    print("Done!")
+
+
+if __name__ == "__main__":
     main()
