026506d873ab38991df1a1e10ae733c3ef0524b6
Ryota MAEDA <maenosuke079@gmail.com>
2024-06-05 22:35:40 +0900

    Update calculate_mueller.py

--- examples/calculate_mueller.py
+++ examples/calculate_mueller.py
@@ -3,42 +3,66 @@
 Download dataset from here.
 - https://drive.google.com/drive/folders/1W66tMue6xi0F1QSG9_sDEgeukV-8QkZs?usp=sharing
 """
-import argparse
+
+import numpy as np
+import cv2
 import polanalyser as pa
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", type=str, default="dataset/toy_example_3x3_pc")
-    args = parser.parse_args()
-
-    path = args.input
+    path = "dataset/mm3x3_example"
     print(f"Load images from '{path}'")
-    pcontainer = pa.PolarizationContainer(path)
 
-    print("Calculate Mueller matrix")
-    image_list = pcontainer.get_list("image")
-    mueller_psg_list = pcontainer.get_list("mueller_psg")
-    mueller_psa_list = pcontainer.get_list("mueller_psa")
-    img_mueller = pa.calcMueller(image_list, mueller_psg_list, mueller_psa_list)
+    images, props = pa.imreadMultiple(path)
+    mm_psg = props["mueller_psg"]
+    mm_psa = props["mueller_psa"]
 
-    print(img_mueller.shape, img_mueller.dtype)
+    print(mm_psa.shape, mm_psa.dtype)
+
+    img_mueller = pa.calcMueller(images, mm_psg, mm_psa)
+
+    print(img_mueller.shape, img_mueller.dtype)  # (2048, 2448, 3, 3) float64
+
+    # Normalized by a maximum value for visualization
+    # Practical Tip: Use np.percentile() not to be affected by outliers.
+    img_mueller_maxnorm = np.clip(img_mueller / np.percentile(np.abs(img_mueller), 99), -1, 1)
+    img_mueller_maxnorm_vis = pa.applyColorMap(img_mueller_maxnorm, colormap="RdBu", vmin=-1, vmax=1)
+    img_mueller_maxnorm_vis_grid = pa.makeGridMueller(img_mueller_maxnorm_vis, border=8)
+    cv2.imwrite("mueller_maxnorm_vis_grid.png", img_mueller_maxnorm_vis_grid)
+
+    # Gamma correction for visualization
+    img_mueller_gamma = img_mueller_maxnorm
+    img_mueller_gamma = pa.gammaCorrection(img_mueller_gamma)
+    img_mueller_gamma_vis = pa.applyColorMap(img_mueller_gamma, colormap="RdBu", vmin=-1, vmax=1)
+    img_mueller_gamma_vis_grid = pa.makeGridMueller(img_mueller_gamma_vis, border=8)
+    cv2.imwrite("mueller_gamma_vis_grid.png", img_mueller_gamma_vis_grid)
 
     # Normalized by m00 for visualization
-    img_mueller_normalized = img_mueller.copy()
-    n1, n2 = img_mueller_normalized.shape[-2:]
-    img_mueller_normalized_m00 = img_mueller_normalized[..., 0, 0]
-    for i in range(n2):
-        for j in range(n1):
-            if i == 0 and j == 0:
-                continue
-            img_mueller_normalized[..., j, i] /= img_mueller_normalized_m00
+    img_mueller_m00norm = img_mueller / img_mueller[..., 0, 0][..., None, None]
+    img_mueller_m00norm_vis = pa.applyColorMap(img_mueller_m00norm, colormap="RdBu", vmin=-1, vmax=1)
+    img_mueller_m00norm_vis_grid = pa.makeGridMueller(img_mueller_m00norm_vis, border=8)
+    cv2.imwrite("mueller_m00norm_vis_grid.png", img_mueller_m00norm_vis_grid)
 
-    filename_visalize = "plot_mueller.png"
-    filename_visalize_normalized = "plot_mueller_normalized.png"
-    print(f"Visualize and export to '{filename_visalize}' and '{filename_visalize_normalized}'")
-    pa.plotMueller(filename_visalize, img_mueller, vabsmax=2.0)
-    pa.plotMueller(filename_visalize_normalized, img_mueller_normalized, vabsmax=1.0)
+    # Posi/Nega visualization
+    img_mueller_posi_nega = img_mueller_maxnorm * 2
+    img_mueller_posi = np.clip(img_mueller_posi_nega, 0, 1)
+    img_mueller_nega = np.clip(-img_mueller_posi_nega, 0, 1)
+    img_mueller_posi_vis = np.clip(img_mueller_posi * 255, 0, 255).astype(np.uint8)
+    img_mueller_nega_vis = np.clip(img_mueller_nega * 255, 0, 255).astype(np.uint8)
+    img_mueller_posi_vis_grid = pa.makeGridMueller(img_mueller_posi_vis, border_color=[172, 102, 33], border=32)
+    img_mueller_nega_vis_grid = pa.makeGridMueller(img_mueller_nega_vis, border_color=[42, 24, 178], border=32)
+    img_mueller_posi_nega_vis_grid = pa.makeGrid([img_mueller_posi_vis_grid, img_mueller_nega_vis_grid], nrow=1, ncol=2, border=0)
+    cv2.imwrite("mueller_posi_vis_grid.png", img_mueller_posi_nega_vis_grid)
+
+    # RdBu colorbar
+    w, h = 16, 256
+    img_bar = np.linspace(1, -1, h)[..., None] * np.ones((1, w))
+    img_colorbar = pa.applyColorMap(img_bar, colormap="RdBu", vmin=-1, vmax=1)
+    cv2.imwrite("colorbar_RdBu.png", img_colorbar)
+
+    img_bar_gamma = pa.gammaCorrection(img_bar)
+    img_colorbar_gamma = pa.applyColorMap(img_bar_gamma, colormap="RdBu", vmin=-1, vmax=1)
+    cv2.imwrite("colorbar_gamma_RdBu.png", img_colorbar_gamma)
 
 
 if __name__ == "__main__":
