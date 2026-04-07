6807215cf4c26e112a4b3e961c3202cde85a6c7e
Ryota MAEDA <maenosuke079@gmail.com>
2026-04-07 15:17:01 +0900

    Update __init__.py

--- polanalyser/__init__.py
+++ polanalyser/__init__.py
@@ -2,18 +2,11 @@ import os
 
 os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
 
-from .stokes import *
-from .mueller import *
-from .demosaicing import *
-from .visualization import *
-from .io import *
 from . import random
+from .compat import *
+from .demosaicing import *
+from .io import *
+from .mueller import *
 from .spectrum import spectrum_to_color
-from .pbrdf import MeasuredPolarimetricBRDF
-
-
-# Alias for old style
-gammaCorrection = gamma
-load_pbsdf = pbrdf.load
-save_pbsdf = pbrdf.save
+from .stokes import *
 from .vis import *
